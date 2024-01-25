ee01eb203b738f3f6564d27ba9545f2bffc2b3c0
Brandyn Hamilton <brandynham1120@gmail.com>
2024-01-25 10:31:15 -0500

    testing homepage

--- app/.ipynb_checkpoints/home_page-checkpoint.py
+++ app/.ipynb_checkpoints/home_page-checkpoint.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from lido_page import ldo_market_value, lido_cagr, current_ratio as ldo_current_ratio,  enterprise_value as lido_ev, ldo_avg_excess_return, ev_to_rev as lido_ev_rev, ldo_dpi_long_wacc, ldo_dpi_short_wacc, ldo_eth_long_wacc, ldo_eth_short_wacc
+from .lido_page import ldo_market_value, lido_cagr, current_ratio as ldo_current_ratio,  enterprise_value as lido_ev, ldo_avg_excess_return, ev_to_rev as lido_ev_rev, ldo_dpi_long_wacc, ldo_dpi_short_wacc, ldo_eth_long_wacc, ldo_eth_short_wacc
 from maker_page import market_value as mkr_market_value, mkr_cagr, current_ratio as mkr_current_ratio, enterprise_value as mkr_ev, mkr_avg_excess_return, ev_to_rev as mkr_ev_rev, mkr_dpi_long_wacc, mkr_dpi_short_wacc, mkr_eth_long_wacc, mkr_eth_short_wacc
 from data.makerdao import wacc as mkr_wacc, market_to_book as mkr_mtb
 from data.Lido import wacc as lido_wacc, market_to_book as ldo_mtb
